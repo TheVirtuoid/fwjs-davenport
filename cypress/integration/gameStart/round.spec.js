6544bc5b31fb23dfc591db692d709f0b54c2507c
TheVirtuoid <thevirtuoid@gmail.com>
2022-04-25 22:04:57 -0400

    Added first round of tests.

---
+++ cypress/integration/gameStart/round.spec.js
@@ -0,0 +1,12 @@
+import { StandardCard, StandardCardSuits, StandardCardRanks } from "@virtuoid/standard-card";
+import Deck from "@virtuoid/deck";
+
+describe('round play', () => {
+	it('should insure that there are at least 2 players', () => {});
+	it('should insure that there are no more than 7 players', () => {});
+	it('should deal out 5 cards to each player', () => {});
+	it('should shuffle in the discard deck if there are not enough cards to deal', () => {});
+	it('should collect the played cards from the players and determine a winner', () => {});
+	it('should shuffle in the discard deck if there are not enough cards to replenish the loser hands', () => {});
+	it('should replenish the loser hands with a single card', () => {});
+});
