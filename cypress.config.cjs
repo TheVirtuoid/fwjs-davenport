271d09a6250fc69e3f13ae3c01edf56babca38ab
TheVirtuoid <thevirtuoid@gmail.com>
2022-07-01 18:15:10 -0400

    Upgraded to cypress 10.3.0 (3rd try)

---
+++ cypress.config.cjs
@@ -0,0 +1,29 @@
+const { defineConfig } = require('cypress')
+const webpackPreprocessor = require('@cypress/webpack-preprocessor');
+const webpackOptions= webpackPreprocessor.defaultOptions;
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('file:preprocessor', webpackPreprocessor(webpackOptions));
+    },
+    supportFile: false
+  }
+});
+
+/*
+import { defineConfig } from 'cypress'
+import webpackPreprocessor from '@cypress/webpack-preprocessor';
+console.log(webpackPreprocessor);
+export default defineConfig({
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return webpackPreprocessor(on, config);
+      // return require('./cypress/plugins/index.cjs')(on, config)
+      // return import('./cypress/plugins/index.cjs')(on, config);
+    },
+    supportFile: false
+  },
+})
+*/
